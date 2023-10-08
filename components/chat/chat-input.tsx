0498c233b6e83c17588097327f84c315ccb48743
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-10-08 13:42:44 -0600

    [add] message attachment

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -15,6 +15,7 @@ import {
   FormItem,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -33,6 +34,7 @@ export const ChatInput = ({
   name,
   type,
 }: ChatInputProps) => {
+  const { onOpen } = useModal();
   const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -72,6 +74,7 @@ export const ChatInput = ({
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
